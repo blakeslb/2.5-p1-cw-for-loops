c6b8d74c7a0ef24c332d97889bbb50e88da53090
Bianca M Blakesley <75644837+blakeslb@users.noreply.github.com>
2022-03-06 16:13:28 -0500

    Add files via upload

---
+++ 2.5-p1-cw-for-loops/script.js
@@ -0,0 +1,24 @@
+// 1. Make a loop that counts from 0 to 10.  It should print out "The number is" and each integer, from 0 to 10, in the console.
+
+
+// 2. Make a loop that counts from 5 to 15 in the console, in multiples of 3. It should print out the number "is a multiple of 3."
+
+
+// 3. Make a loop that counts backwards from 100 to 0, and prints out in the console.
+console.log('Count down from 100:')
+
+
+
+// 4. Make a loop that counts backwards from 100 to 0 in increments of 5, and prints out in the console.
+console.log('Count down from 100:')
+
+
+// 5. Write a function that takes in two integers parameters and returns the multiplication of those two values. Print the value to the console.
+
+
+// 6. Write a function that takes in a string parameter and returns true if the string contains 'a', otherwise the function returns false.
+
+
+// 7. Write a function that takes in a number and prints the multiplication table of that number(multiply the number by 0 - 10).
+
+//Done!
